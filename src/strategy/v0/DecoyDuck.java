656f964c6994b92e8249c78e73913a370d381ed2
robertvunabandi <rvunabandi@gmail.com>
2021-08-10 00:07:39 -0400

    fix copy/paste typo in decoy duck

--- src/strategy/v0/DecoyDuck.java
+++ src/strategy/v0/DecoyDuck.java
@@ -4,15 +4,15 @@ public class DecoyDuck extends Duck {
     public DecoyDuck(String name) {
         super(name);
     }
-    
+
     @Override
     public void quack() {
         // Decoy ducks shouldn't be able to quack, so this will do nothing.
     }
-    
+
     @Override
     public void display() {
-        System.out.printf("I'm a rubber duck, and my name is %s%n", name);
+        System.out.printf("I'm a decoy duck, and my name is %s%n", name);
     }
 
     @Override
