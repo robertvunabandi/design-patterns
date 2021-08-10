0a9eb372c8ba56611ea5f5f8edb75aa258d167bd
robertvunabandi <rvunabandi@gmail.com>
2021-08-09 23:51:10 -0400

    add decoy ducks
    Adding decoy ducks which are fake ducks. These ducks,
    however, cannot quack nor fly. For that reason, I've
    overriden these methods to do nothing.

---
+++ src/strategy/bad/DecoyDuck.java
@@ -0,0 +1,22 @@
+package strategy.bad;
+
+public class DecoyDuck extends Duck {
+    public DecoyDuck(String name) {
+        super(name);
+    }
+    
+    @Override
+    public void quack() {
+        // Decoy ducks shouldn't be able to quack, so this will do nothing.
+    }
+    
+    @Override
+    public void display() {
+        System.out.printf("I'm a rubber duck, and my name is %s%n", name);
+    }
+
+    @Override
+    public void fly() {
+        // Decoy ducks shouldn't be able to fly, so this will do nothing.
+    }
+}
