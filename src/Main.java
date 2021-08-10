c3fc9105640acc36e330ec3ae8d2577f4a4c0c7c
robertvunabandi <rvunabandi@gmail.com>
2021-08-09 22:51:37 -0400

    add Main file to run scripts

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2021 Robert M. Vunabandi.
+ *
+ * This work is licensed under the terms of the MIT license. For a copy,
+ * @see <a href="https://opensource.org/licenses/MIT">MIT License</a>
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
